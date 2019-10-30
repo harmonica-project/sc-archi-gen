cd06d38c8e48db7c21f26b3457903f8e34a3d4b1
nicoSix <nicolas.six.fr@gmail.com>
2019-10-30 10:17:03 +0100

    modifying vagrant config henerator to add generation of truffle config js

---
+++ vagrant/generate_config.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+
+var nbNodes = parseInt(process.argv.slice(2));
+var ipList = []
+
+for(var i = 1; i <= nbNodes; i++) {
+    ipList.push({
+        ip: '10.0.0.' + (i + 10),
+        user: 'vagrant',
+        password: 'vagrant'
+    })
+}
+
+fs.writeFileSync("../ip_list.json", JSON.stringify(ipList));
+
+var truffleConfig = {
+    networks: {
+        infra: {
+            host: "172.16.72.5",     
+            port: 8545,            
+            network_id: 61997,       
+            gasLimit: "0x346DC5D638865",
+            gasPrice: "0x0",
+            },
+        },
+  
+        compilers: {
+            solc: {
+                version: "0.5.0"
+            }
+        }
+    }
+
+    fs.writeFileSync("../truffle-config.js", 'module.exports = ' + JSON.stringify(truffleConfig));
